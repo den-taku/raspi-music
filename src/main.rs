c607e059699d38031230ceb764e00c3dfa83c79a
den-taku <dentaku1412@gmail.com>
2022-06-23 00:29:54 +0900

    Fix name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use ears::{Sound, AudioController};
 
 fn main() {
-    let mut sound = Sound::new("./sample/sample.wav").unwrap();
+    let mut sound = Sound::new("./samples/sample.wav").unwrap();
     sound.play();
 
     while sound.is_playing() {}
