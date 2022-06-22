09f11e189603b6d57aecd6704f37fa9ce23fd86c
den-taku <dentaku1412@gmail.com>
2022-06-23 00:09:41 +0900

    Add basis

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use alto::{Alto, AltoResult};
+
+fn main() -> AltoResult<()> {
+    let alto = Alto::load_default()?;
+
+    for s in alto.enumerate_outputs() {
+        println!("Found device: {}", s.to_str().unwrap());
+    }
+
+    let device = alto.open(None)?;
+    let context = device.new_context(None)?;
+
+    context.set_position([1.0, 4.0, 5.0])?;
+    context.set_velocity([2.5, 0.0, 0.0])?;
+    context.set_orientation(([0.0, 0.0, 1.0], [0.0, 1.0, 0.0]))?;
+
+    let _source = context.new_static_source()?;
+
+    Ok(())
 }
